b213d9d1c442ace91887ebcc68240bd3a79ca089
Wiz-Amit <ahazratwt@gmail.com>
2020-06-07 11:16:47 +0530

    fixed wrong update date

--- recovery-and-death.js
+++ recovery-and-death.js
@@ -1,38 +1,46 @@
 const app = new Vue({
-  el: "#recovery-and-death",
+  el: '#recovery-and-death',
   data: {
-    orderByKey: "cr",
-    filterByKey: "",
+    orderByKey: 'cr',
+    filterByKey: '',
+    rawRecords: [],
     records: [],
   },
   mounted: function () {
-    fetch("https://api.covid19india.org/data.json")
+    fetch('https://api.covid19india.org/data.json')
       .then((response) => response.json())
       .then((json) => {
         // console.table(json.statewise);
-        this.records = json.statewise.filter(record => record.state !== 'Total');
+        this.rawRecords = json.statewise;
+
+        this.records = this.rawRecords.filter(
+          (r) =>
+            r.state.toLowerCase() !== 'Total'.toLowerCase() &&
+            r.state.toLowerCase() !== 'State Unassigned'.toLowerCase()
+        );
       });
   },
   computed: {
     td: function () {
       let data = this.records;
+
       data.forEach((row) => {
         row.cr =
-          "00" +
+          '00' +
           Math.round(
             ((parseInt(row.recovered) + 1) /
               (parseInt(row.recovered) + parseInt(row.deaths) + 2)) *
               100
           ) +
-          "%";
+          '%';
         row.cd =
-          "00" +
+          '00' +
           Math.round(
             ((parseInt(row.deaths) + 1) /
               (parseInt(row.recovered) + parseInt(row.deaths) + 2)) *
               100
           ) +
-          "%";
+          '%';
 
         row.cr = row.cr.slice(row.cr.length - 4);
         row.cd = row.cd.slice(row.cd.length - 4);
@@ -40,22 +48,20 @@ const app = new Vue({
       if (this.filterByKey) {
         data = data.filter((row) => row.state == this.filterByKey);
       }
-      return _.orderBy(data, this.orderByKey, "desc");
+      return _.orderBy(data, this.orderByKey, 'desc');
     },
-    modifiedAt: function() {
-      if(!this.records[0]) return 'Unknown';
-      timeStamps = this.records.map(record => record.lastupdatedtime);
-      latestTime = timeStamps.reduce((latest, current) => {
-        // console.log({latest, current: current});
-        return latest > current ? latest : current;
-      });
+    modifiedAt: function () {
+      if (!this.records[0]) return 'Unknown';
+      const latestTime = this.rawRecords.find(
+        (r) => r.state.toLowerCase() === 'Total'.toLowerCase()
+      ).lastupdatedtime;
 
       return latestTime.slice(0, latestTime.length - 9);
-    }
+    },
   },
   methods: {
     orderBy: function (key) {
-      if (this.orderByKey == key) this.orderByKey = "";
+      if (this.orderByKey == key) this.orderByKey = '';
       else this.orderByKey = key;
     },
   },
